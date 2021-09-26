63191c81103a0f81438c34f52b3d2746e3bf331d
Anna Eriksson <anna.eriksson@claremont.se>
2021-09-26 21:05:15 +0200

    init firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyC7mUGQzUxU_mfqog_9tUzyEOEYbio5GVg',
+//   authDomain: 'auth-7702a.firebaseapp.com',
+//   projectId: 'auth-7702a',
+//   storageBucket: 'auth-7702a.appspot.com',
+//   messagingSenderId: '460641117576',
+//   appId: '1:460641117576:web:7b58b2893063b8fdf3863d',
+//   measurementId: 'G-JRQKCPEX4R'
+// };
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+
 function App() {
-  return <div className='App'></div>;
+  return <div className='bg-green-600 h-full'></div>;
 }
 
 export default App;
