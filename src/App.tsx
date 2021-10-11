1b40764de4296b80d397883f42df2fac2791a534
Anna Eriksson <anna.eriksson@claremont.se>
2021-10-11 22:08:35 +0200

    feat: sign in feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,87 @@
-import React from 'react';
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyC7mUGQzUxU_mfqog_9tUzyEOEYbio5GVg',
-//   authDomain: 'auth-7702a.firebaseapp.com',
-//   projectId: 'auth-7702a',
-//   storageBucket: 'auth-7702a.appspot.com',
-//   messagingSenderId: '460641117576',
-//   appId: '1:460641117576:web:7b58b2893063b8fdf3863d',
-//   measurementId: 'G-JRQKCPEX4R'
-// };
-
-// // Initialize Firebase
-// // const app = initializeApp(firebaseConfig);
-// // const analytics = getAnalytics(app);
+import React, { useState } from 'react';
+import firebase from 'firebase/compat/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
 
 function App() {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyC7mUGQzUxU_mfqog_9tUzyEOEYbio5GVg',
+    authDomain: 'auth-7702a.firebaseapp.com',
+    projectId: 'auth-7702a',
+    storageBucket: 'auth-7702a.appspot.com',
+    messagingSenderId: '460641117576',
+    appId: '1:460641117576:web:7b58b2893063b8fdf3863d',
+    measurementId: 'G-JRQKCPEX4R'
+  };
+  firebase.initializeApp(firebaseConfig);
+  const auth = getAuth();
+
+  const [mail, setMail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  // createUserWithEmailAndPassword(auth, 'aaa@aaa.se', 'psw123')
+  //   .then(userCredential => {
+  //     // Signed in
+  //     const user = userCredential.user;
+  //     console.log(user);
+  //     // ...
+  //   })
+  //   .catch(error => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // ..
+  //   });
+
+  const handleMailInput = (e: React.FormEvent<HTMLInputElement>) =>
+    setMail(e.currentTarget.value);
+
+  const handlePasswordInput = (e: React.FormEvent<HTMLInputElement>) =>
+    setPassword(e.currentTarget.value);
+
+  const handleLogin = () => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, mail, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
   return (
-    <div style={{ height: '100px' }} className='bg-green-600'>
-      <h2>test</h2>
+    <div className='bg-yellow-900	opacity-50 min-h-screen flex items-center justify-center '>
+      <div>
+        <h1 className='text-white text-center mb-10 sm:mb-20'>
+          Welcome to Anna's wonderland
+        </h1>
+        <div className='h-1/2 w-3/4 flex flex-col border-2 rounded-3xl px-8 py-10 sm:py-20 m-auto'>
+          <p className='text-white text-lg'>Login</p>
+          <input
+            className='h-8 rounded-3xl p-4 my-2'
+            placeholder='mail'
+            onChange={handleMailInput}
+          />
+          <input
+            className='h-8 rounded-3xl p-4 my-2'
+            placeholder='password'
+            onChange={handlePasswordInput}
+          />
+          <div className='flex w-full justify-end'>
+            <button
+              className='bg-white font-bold w-max px-4 rounded-3xl border-2 text-yellow-900'
+              onClick={handleLogin}
+            >
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
